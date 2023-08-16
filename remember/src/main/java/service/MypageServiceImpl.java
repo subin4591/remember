f1251d73c58114bd08f661d8f01bea3f948885a5
EUNSE <EUNSE@DESKTOP-LKO2P5J>
2023-08-16 17:09:29 +0900

    8월 16일 메인 병합

--- remember/src/main/java/service/MypageServiceImpl.java
+++ remember/src/main/java/service/MypageServiceImpl.java
@@ -1,10 +1,12 @@
 package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dao.MypageDAO;
 import dto.UserDTO;
 
+@Service
 public class MypageServiceImpl implements MypageService {
 	@Autowired
 	MypageDAO mypagedao;
